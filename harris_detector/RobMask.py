ab5449711a105b4e2e11e8da0f2307ee419b3459
Rob <rob.lass@gmail.com>
2013-04-27 15:48:26 -0400

    added sample input and output.  pep8 changes to harris corner detector.

--- harris_detector/RobMask.py
+++ harris_detector/RobMask.py
@@ -1,6 +1,6 @@
 #!/sw/bin/python
 
-#    RobMask.py - Module for performing convolution 
+#    RobMask.py - Module for performing convolution
 #    Copyright (C) 2005  Rob Lass
 #
 #    This program is free software; you can redistribute it and/or modify
@@ -16,113 +16,111 @@
 #    along with this program; if not, write to the Free Software
 #    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-import string, math
+import string
+import math
 from math import fabs
 from PIL import Image
 
+
 class RobMask:
-	xmask = []
-	ymask = []
-	threshold = 100
+    xmask = []
+    ymask = []
+    threshold = 100
 
-	def __init__(self, filename, threshold):
-		"Takes the name of a file specifying coefficients"
+    def __init__(self, filename, threshold):
+        "Takes the name of a file specifying coefficients"
 
-		self.threshold = threshold
+        self.threshold = threshold
 
-		# read the file, and store coefficients in mask
-		f = open(filename, 'r')
-		line = f.readline()
-		while line != '':
-			numbers = string.split(line)
-			for i in range(len(numbers)):
-				numbers[i] = int(numbers[i])
-			self.xmask.append(string.split(line))
-			line = f.readline()	
-		f.close()
+        # read the file, and store coefficients in mask
+        f = open(filename, 'r')
+        line = f.readline()
+        while line != '':
+            numbers = string.split(line)
+            for i in range(len(numbers)):
+                numbers[i] = int(numbers[i])
+            self.xmask.append(string.split(line))
+            line = f.readline()
+        f.close()
 
-		# verify that the mask is valid
-		if len(self.xmask) != 3:
-			raise "Invalid coefficient input file ", filename
-		for row in self.xmask:
-			if len(row) != 3:
-				raise "Invalid coefficient input file ", filename
+        # verify that the mask is valid
+        if len(self.xmask) != 3:
+            raise "Invalid coefficient input file ", filename
+        for row in self.xmask:
+            if len(row) != 3:
+                raise "Invalid coefficient input file ", filename
 
-		# make the y-mask the inverse of the x-mask
-		for i in range(len(self.xmask)):
-			row = []
-			for j in range(len(self.xmask)):
-				row.append(self.xmask[j][i])
-			self.ymask.append(row)
+        # make the y-mask the inverse of the x-mask
+        for i in range(len(self.xmask)):
+            row = []
+            for j in range(len(self.xmask)):
+                row.append(self.xmask[j][i])
+            self.ymask.append(row)
 
-	def getXPD(self, source):
-	 	"""compute p.d. with respect to x"""
-		gradient = Image.new('L', source.size)
+    def getXPD(self, source):
+        """compute p.d. with respect to x"""
+        gradient = Image.new('L', source.size)
 
-		print "computing x p.d."
+        print "computing x p.d."
 
-		for x in range(1, source.size[0]-1):
-			for y in range(1, source.size[1]-1):
-				Mx = int(self.xmask[0][2]) * source.getpixel((x+1, y-1))
-				Mx = Mx + int(self.xmask[1][2]) * source.getpixel((x+1, y))
-				Mx = Mx + int(self.xmask[2][2]) * source.getpixel((x+1, y+1))
-				Mx = Mx + int(self.xmask[0][0]) * source.getpixel((x-1, y-1))
-				Mx = Mx + int(self.xmask[1][0]) * source.getpixel((x-1, y))
-				Mx = Mx + int(self.xmask[2][0]) * source.getpixel((x-1, y+1))
+        for x in range(1, source.size[0]-1):
+            for y in range(1, source.size[1]-1):
+                Mx = int(self.xmask[0][2]) * source.getpixel((x+1, y-1))
+                Mx = Mx + int(self.xmask[1][2]) * source.getpixel((x+1, y))
+                Mx = Mx + int(self.xmask[2][2]) * source.getpixel((x+1, y+1))
+                Mx = Mx + int(self.xmask[0][0]) * source.getpixel((x-1, y-1))
+                Mx = Mx + int(self.xmask[1][0]) * source.getpixel((x-1, y))
+                Mx = Mx + int(self.xmask[2][0]) * source.getpixel((x-1, y+1))
 
-				gradient.putpixel((x,y), fabs(Mx))
+                gradient.putpixel((x, y), fabs(Mx))
 
-		return gradient
+        return gradient
 
+    def getYPD(self, source):
+        """compute p.d. with respect to y"""
+        gradient = Image.new('L', source.size)
 
+        print "computing y p.d."
 
-	def getYPD(self, source):
-		"""compute p.d. with respect to y"""
-		gradient = Image.new('L', source.size)
-		
-		print "computing y p.d."
+        for x in range(1, source.size[0]-1):
+            for y in range(1, source.size[1]-1):
+                My = int(self.ymask[2][0]) * source.getpixel((x-1, y+1))
+                My = My + int(self.ymask[2][1]) * source.getpixel((x, y+1))
+                My = My + int(self.ymask[2][2]) * source.getpixel((x+1, y+1))
+                My = My + int(self.ymask[0][0]) * source.getpixel((x-1, y-1))
+                My = My + int(self.ymask[0][1]) * source.getpixel((x, y-1))
+                My = My + int(self.ymask[0][2]) * source.getpixel((x+1, y-1))
 
-		for x in range(1, source.size[0]-1):
-			for y in range(1, source.size[1]-1):
-			   	My = int(self.ymask[2][0]) * source.getpixel((x-1, y+1))
-				My = My + int(self.ymask[2][1]) * source.getpixel((x, y+1))
-				My = My + int(self.ymask[2][2]) * source.getpixel((x+1, y+1))
-				My = My + int(self.ymask[0][0]) * source.getpixel((x-1, y-1))
-				My = My + int(self.ymask[0][1]) * source.getpixel((x, y-1))
-				My = My + int(self.ymask[0][2]) * source.getpixel((x+1, y-1))
+                gradient.putpixel((x, y), fabs(My))
 
-				gradient.putpixel((x,y), fabs(My))
+        return gradient
 
-		return gradient
+    def apply(self, mask, source):
+        "Applies the given mask to the source image, and returns edge image"
 
-	def apply(self, mask, source):
-		"Applies the given mask to the source image, and returns edge image"
+        edge_file = Image.new('RGBA', source.size)
 
-		edge_file = Image.new('RGBA', source.size)
+        #convolution
+        for x in range(1, source.size[0]-1):
+            for y in range(1, source.size[1]-1):
+                 # compute p.d. with respect to x
+                Mx = int(self.xmask[0][2]) * source.getpixel((x+1, y-1))
+                Mx = Mx + int(self.xmask[1][2]) * source.getpixel((x+1, y))
+                Mx = Mx + int(self.xmask[2][2]) * source.getpixel((x+1, y+1))
+                Mx = Mx + int(self.xmask[0][0]) * source.getpixel((x-1, y-1))
+                Mx = Mx + int(self.xmask[1][0]) * source.getpixel((x-1, y))
+                Mx = Mx + int(self.xmask[2][0]) * source.getpixel((x-1, y+1))
 
-		#convolution
-		for x in range(1, source.size[0]-1):
-			for y in range(1, source.size[1]-1):
-				 # compute p.d. with respect to x
-				Mx = int(self.xmask[0][2]) * source.getpixel((x+1, y-1))
-				Mx = Mx + int(self.xmask[1][2]) * source.getpixel((x+1, y))
-				Mx = Mx + int(self.xmask[2][2]) * source.getpixel((x+1, y+1))
-				Mx = Mx + int(self.xmask[0][0]) * source.getpixel((x-1, y-1))
-				Mx = Mx + int(self.xmask[1][0]) * source.getpixel((x-1, y))
-				Mx = Mx + int(self.xmask[2][0]) * source.getpixel((x-1, y+1))
+                # compute p.d. with respect to y
+                My = int(self.ymask[2][0]) * source.getpixel((x-1, y+1))
+                My = My + int(self.ymask[2][1]) * source.getpixel((x, y+1))
+                My = My + int(self.ymask[2][2]) * source.getpixel((x+1, y+1))
+                My = My + int(self.ymask[0][0]) * source.getpixel((x-1, y-1))
+                My = My + int(self.ymask[0][1]) * source.getpixel((x, y-1))
+                My = My + int(self.ymask[0][2]) * source.getpixel((x+1, y-1))
 
-				# compute p.d. with respect to y
-			   	My = int(self.ymask[2][0]) * source.getpixel((x-1, y+1))
-				My = My + int(self.ymask[2][1]) * source.getpixel((x, y+1))
-				My = My + int(self.ymask[2][2]) * source.getpixel((x+1, y+1))
-				My = My + int(self.ymask[0][0]) * source.getpixel((x-1, y-1))
-				My = My + int(self.ymask[0][1]) * source.getpixel((x, y-1))
-				My = My + int(self.ymask[0][2]) * source.getpixel((x+1, y-1))
+                mag = math.sqrt(Mx*Mx + My*My)
 
-				mag = math.sqrt(Mx*Mx + My*My)
-
-				if mag > self.threshold:
-					edge_file.putpixel((x,y), (0, int(mag), 0))
-		return edge_file
-
-
+                if mag > self.threshold:
+                    edge_file.putpixel((x, y), (0, int(mag), 0))
+        return edge_file
--- harris_detector/corner_harris.py
+++ harris_detector/corner_harris.py
@@ -5,7 +5,7 @@
 #
 #    The method I use to compute the eigenvalues in this program is explained
 #    here:
-# 		http://www.selu.edu/Academics/Depts/Math/students/george3/george.htm
+#       http://www.selu.edu/Academics/Depts/Math/students/george3/george.htm
 
 
 #    Copyright (C) 2005  Rob Lass
@@ -23,114 +23,119 @@
 #    along with this program; if not, write to the Free Software
 #    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-import sys,os
+import sys
+import os
 from PIL import Image
 from RobMask import RobMask
 from math import sqrt, fabs
 
 threshold = 2000000000
 
-def computeMatrix(image, x, y, x_grad, y_grad, C):
-	"""returns variable 'C' """
-	# upper left
-	C[0][0] = C[0][0] + x_grad.getpixel((x,y))**2
-	C[0][1] = C[0][1] + x_grad.getpixel((x,y))*y_grad.getpixel((x,y))
-	C[1][0] = C[1][0] + x_grad.getpixel((x,y))*y_grad.getpixel((x,y))
-	C[1][1] = C[1][1] + y_grad.getpixel((x,y))**2
 
-	return C
+def computeMatrix(image, x, y, x_grad, y_grad, C):
+    """returns variable 'C' """
+    # upper left
+    C[0][0] = C[0][0] + x_grad.getpixel((x, y))**2
+    C[0][1] = C[0][1] + x_grad.getpixel((x, y))*y_grad.getpixel((x, y))
+    C[1][0] = C[1][0] + x_grad.getpixel((x, y))*y_grad.getpixel((x, y))
+    C[1][1] = C[1][1] + y_grad.getpixel((x, y))**2
+
+    return C
+
 
 def computeGradients(image):
-	"""returns (x,y) where x and y are gradients in their respective
-	directions"""
+    """returns (x,y) where x and y are gradients in their respective
+    directions"""
 
-	# use prewitt to compute gradients
-	m = RobMask("prewitt.coe", 0)
-	x_grad,y_grad = m.getXPD(image), m.getYPD(image)
+    # use prewitt to compute gradients
+    m = RobMask("prewitt.coe", 0)
+    x_grad, y_grad = m.getXPD(image), m.getYPD(image)
 
-	good_pixels = []
-	# for every point, window size = 3
-	print "computing response"
-	for x in range(1, image.size[0]-1):
-		for y in range(1, image.size[1]-1):
-			# compute C
-			C = [[0,0],[0,0]]
+    good_pixels = []
+    # for every point, window size = 3
+    print "computing response"
+    for x in range(1, image.size[0]-1):
+        for y in range(1, image.size[1]-1):
+            # compute C
+            C = [[0, 0], [0, 0]]
 
-			# first row
-			C = computeMatrix(image, x-1, y-1, x_grad, y_grad, C)
-			C = computeMatrix(image, x, y-1, x_grad, y_grad, C)
-			C = computeMatrix(image, x+1, y-1, x_grad, y_grad, C)
+            # first row
+            C = computeMatrix(image, x-1, y-1, x_grad, y_grad, C)
+            C = computeMatrix(image, x, y-1, x_grad, y_grad, C)
+            C = computeMatrix(image, x+1, y-1, x_grad, y_grad, C)
 
-			# second row
-			C = computeMatrix(image, x-1, y, x_grad, y_grad, C)
-			C = computeMatrix(image, x, y, x_grad, y_grad, C)
-			C = computeMatrix(image, x+1, y, x_grad, y_grad, C)
+            # second row
+            C = computeMatrix(image, x-1, y, x_grad, y_grad, C)
+            C = computeMatrix(image, x, y, x_grad, y_grad, C)
+            C = computeMatrix(image, x+1, y, x_grad, y_grad, C)
 
-			#third row
-			C = computeMatrix(image, x-1, y+1, x_grad, y_grad, C)
-			C = computeMatrix(image, x, y+1, x_grad, y_grad, C)
-			C = computeMatrix(image, x+1, y+1, x_grad, y_grad, C)
+            #third row
+            C = computeMatrix(image, x-1, y+1, x_grad, y_grad, C)
+            C = computeMatrix(image, x, y+1, x_grad, y_grad, C)
+            C = computeMatrix(image, x+1, y+1, x_grad, y_grad, C)
 
-			# compute lambda for this matrix.  
-			t = C[0][0]
-			a = C[0][1]
-			b = C[1][0]
-			c = C[1][1]
+            # compute lambda for this matrix.
+            t = C[0][0]
+            a = C[0][1]
+            b = C[1][0]
+            c = C[1][1]
 
-			# make sure we have a rational root
-			if t*t - 2*c*t + c*c - 4*a*b > 0:
-				# see reference at the top for explanation of this
-				l_1 = (t + c - sqrt(t*t - 2*c*t + c*c - 4*a*b))/2
-				l_2 = (t + c + sqrt(t*t - 2*c*t + c*c - 4*a*b))/2
+            # make sure we have a rational root
+            if t*t - 2*c*t + c*c - 4*a*b > 0:
+                # see reference at the top for explanation of this
+                l_1 = (t + c - sqrt(t*t - 2*c*t + c*c - 4*a*b))/2
+                l_2 = (t + c + sqrt(t*t - 2*c*t + c*c - 4*a*b))/2
 
-				k = 0.06
-				R = l_1 * l_2 - k * (l_1 + l_2)**2
-				if R > threshold:
-					if l_1 < l_2:
-						l_2 = l_1
-					good_pixels.append((R, x, y))
+                k = 0.06
+                R = l_1 * l_2 - k * (l_1 + l_2)**2
+                if R > threshold:
+                    if l_1 < l_2:
+                        l_2 = l_1
+                    good_pixels.append((R, x, y))
 
-	return good_pixels
-		
+    return good_pixels
+
+
 def computeGradient(image, u, v):
-	"""computes gradient.  acceptable values for u and v are 0 and 1."""
+    """computes gradient.  acceptable values for u and v are 0 and 1."""
 
-	# new image to hold gradient
-	gradient = Image.new('L', image.size)
+    # new image to hold gradient
+    gradient = Image.new('L', image.size)
 
-	#compute gradient using standard formula
-	for x in range(image.size[0]-1):
-		for y in range(image.size[1]-1):
-			gradient.putpixel((x,y),
-				(image.getpixel((x+u, y+v)) - image.getpixel((x,y)))**2)
+    #compute gradient using standard formula
+    for x in range(image.size[0]-1):
+        for y in range(image.size[1]-1):
+            gradient.putpixel((x, y), (image.getpixel((x+u, y+v)) -
+                              image.getpixel((x, y)))**2)
+
 
 def harrisDetector(filename):
-	"""Runs a Harris edge detector on the specified file and returns a list of
-	points it thinks are corners"""
+    """Runs a Harris edge detector on the specified file and returns a list of
+    points it thinks are corners"""
 
-	image = Image.open(filename).convert('L')
+    image = Image.open(filename).convert('L')
 
-	response = computeGradients(image)
+    response = computeGradients(image)
 
-	# remove adjacent pixels
+    # remove adjacent pixels
 
-	# write corners to image
-	output = Image.new('RGBA', image.size)
-	for  pixels in response:
-		output.putpixel((pixels[1], pixels[2]), (255,0,0))
+    # write corners to image
+    output = Image.new('RGBA', image.size)
+    for pixels in response:
+        output.putpixel((pixels[1], pixels[2]), (255, 0, 0))
 
-	#make output dir if it doesn't exist
-	pwd = os.listdir(".")
-	if "output_images" not in pwd:
-		os.mkdir("output_images")
+    #make output dir if it doesn't exist
+    pwd = os.listdir(".")
+    if "output_images" not in pwd:
+        os.mkdir("output_images")
 
-	# save
-	output.save("output_images/"+os.path.splitext(filename)[0]+"_harris.png")
+    # save
+    output.save("output_images/"+os.path.splitext(filename)[0]+"_harris.png")
 
 # print usage
 if len(sys.argv) < 2:
-	print "Usage: python ", sys.argv[0], " filename(s)"
+    print "Usage: python ", sys.argv[0], " filename(s)"
 
 # run on each input file
 for argv in sys.argv[1:]:
-	harrisDetector(argv)
+    harrisDetector(argv)
